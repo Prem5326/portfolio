e90756aa30b8df55bed20a4cf31b7cbf167a06b9
BUKKE PREM CHANDHAN KUMAR NAIK <premchandhan@gmail.com>
2025-08-01 11:21:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Example: Smooth scroll effect for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
